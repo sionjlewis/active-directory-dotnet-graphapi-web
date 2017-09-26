74cd2267e75d7dd6766ae249ad64873e49aa4d71
Siôn J. Lewis <sionjlewis@users.noreply.github.com>
2017-09-27 09:54:59 +1300

    Added some more fields...
    Added some more fields, including userPrincipalName

--- WebAppGraphAPI/Models/UserProfile.cs
+++ WebAppGraphAPI/Models/UserProfile.cs
@@ -22,8 +22,23 @@ namespace WebAppGraphAPI.Models
         public string Mobile { get; set; }
         public string TelephoneNumber { get; set; }
         public string FacsimileTelephoneNumber { get; set; }
+        /// <summary>
+        /// Email Address.
+        /// </summary>
         public string Mail { get; set; }
         public string EmployeeId { get; set; }
         public string PreferredLanguage { get; set; }
+        /// <summary>
+        /// User Principal Name (UPN) is the name of a system user in an e-mail address format.
+        /// </summary>
+        public string userPrincipalName { get; set; }
+        /// <summary>
+        /// Attribute is separate from Country and is used while assigning a license to a user in Office 365.
+        /// </summary>
+        public string usageLocation { get; set; }
+        /// <summary>
+        /// The SIP address is used by Communications Server or Lync Server to route incoming calls and send voice mail to the user.
+        /// </summary>
+        public string sipProxyAddress { get; set; }
     }
 }
